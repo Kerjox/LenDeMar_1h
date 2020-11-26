49663973392b2bbc128858fb480db686662ee64f
Kerjox <kerjoxgamer095@gmail.com>
2020-11-26 09:22:18 +0100

    26-nov-20

--- Marcas15/main.js
+++ Marcas15/main.js
@@ -1,12 +1,16 @@
+let buttonStart = document.getElementById('startTimer');
+let buttonRotar = document.getElementById('rotar');
+let buttonStop = document.getElementById('stopTimer')
+
 posXimg = new Array("100px", "400px", "700px", "400px");
 posYimg = new Array("700px", "1100px", "760px", "300px");
 
 
 function startTimer() {
 
-    document.getElementById('startTimer').style.display = "none";
-    document.getElementById('rotar').style.visibility = "hidden";
-    document.getElementById('stopTimer').style.display = "";
+    buttonStart.style.display = "none";
+    buttonRotar.style.visibility = "hidden";
+    buttonStop.style.display = "";
     
     rotarInterval = setInterval(function() {
         rotar();
@@ -17,9 +21,9 @@ function startTimer() {
 
 function stopTimer() {
     
-    document.getElementById('startTimer').style.display = "";
-    document.getElementById('rotar').style.visibility = "";
-    document.getElementById('stopTimer').style.display = "none";
+    buttonStart.style.display = "";
+    buttonRotar.style.visibility = "";
+    buttonStop.style.display = "none";
     clearInterval(rotarInterval);
 
 }
--- Marcas20 - Reloj_Digital/main.js
+++ Marcas20 - Reloj_Digital/main.js
@@ -1,34 +1,88 @@
-window.addEventListener("load", () => {
-
-    setInterval(() => {
-        cambiarReloj();
-    }, 1000);
-
-});
+const buttonSetAlarm = document.getElementById('setAlarm');
+const inputAlarmHour = document.getElementById('alarmHours');
+const inputAlarmMinutes = document.getElementById('alarmMinutes');
+let alarmSeted = false;
+let hora; let minuto;
+let alarmHora; let alamMinuto;
 
 function cambiarReloj() {
-
+    
     let date = new Date();
-    let hora = date.getHours();
-    let minuto = date.getMinutes();
+    hora = date.getHours();
+    minuto = date.getMinutes();
     let segundo = date.getSeconds();
     let horaStr = "";
-
+    
     horaStr = (hora < 10) ? "0" + hora : hora;
     horaStr += (minuto < 10) ? ":0" + minuto : ":" + minuto;
     horaStr += (segundo < 10) ? ":0" + segundo : ":" + segundo;
-
+    
     console.log({
         hora: hora,
         minuto: minuto,
-        segundo: segundo, 
+        segundo: segundo,
         horaStr: horaStr
     });
-
+    
     horaBar.style.width = hora / 24 * 100 + "%";
     minutoBar.style.width = minuto / 60 * 100 + "%";
     segundoBar.style.width = segundo / 60 * 100 + "%";
-
+    
     caja.value = date;
     document.getElementById('reloj').innerHTML = horaStr;
+    
+    if (segundo == 0) {
+        
+        checkAlarm()
+    }
+
+    setTimeout(() => {
+        cambiarReloj();
+    }, 1000);
+
+
 }
+
+function checkAlarm() {
+    
+    if (hora == alarmHora && minuto == alamMinuto) {
+        
+        document.getElementById('despetador').style.display = "";
+
+    }
+
+}
+
+buttonSetAlarm.addEventListener("click", () => {
+
+    if (alarmSeted) {
+
+        buttonSetAlarm.innerText = "Set Alarm";
+        buttonSetAlarm.className = "btn btn-success";
+
+        inputAlarmHour.disabled = false;
+        inputAlarmMinutes.disabled = false;
+
+        alarmSeted = false;
+
+    }else{
+    
+        alarmHora = Number(inputAlarmHour.value);
+        alamMinuto = Number(inputAlarmMinutes.value);
+    
+        inputAlarmHour.disabled = true;
+        inputAlarmMinutes.disabled = true;
+
+        buttonSetAlarm.innerText = "Stop Alarm";
+        buttonSetAlarm.className = "btn btn-danger";
+    
+        alarmSeted = true;
+    }
+
+});
+
+window.addEventListener("load", () => {
+
+    cambiarReloj();
+
+});
