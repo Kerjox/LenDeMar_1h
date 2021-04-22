85c2664bcfbb3b1d2b93ea1afb67d7e0ad0e9d57
kerjox <kerjoxgamer095@gmail.com>
2021-04-22 09:47:47 +0200

    Carrito

--- Eva3/Ejercicio05 - Carrito/main.js
+++ Eva3/Ejercicio05 - Carrito/main.js
@@ -1,13 +1,74 @@
 const doc = document;
 const ss = sessionStorage;
+const productsCart = doc.querySelector("#productsCart");
+const totalImport = doc.querySelector("#totalImport");
 const content = doc.querySelector("#content");
 
 
 window.onload = function () {
     
-    mostrar();
+    //mostrar();
+    showCart();
 }
+
+$("table button").click(function (e) {
     
+    let id = $(this).attr("id");
+
+    if (ss.getItem(id) > 0) {
+
+        ss.setItem(id, parseInt(ss.getItem(id)) + 1);
+    } else {
+
+        ss.setItem(id, 1);
+    }
+    showCart();
+});
+
+$("#productsCart").click(function (e) {
+    
+    console.log("GGGGG");
+});
+
+function calcTotalProducts(e) {
+
+    let totalProducts = 0;
+    totalImport.innerHTML = "Total Productos: ";
+
+    for (let i = 0; i < ss.length; i++) {
+
+        totalProducts += parseInt(ss.getItem(ss.key(i)));
+    }
+
+    totalImport.innerHTML += totalProducts;
+}
+
+$("#reset").click(function (e) {
+    
+    ss.clear();
+    showCart();
+});
+
+function showCart() {
+    
+    if (ss.length == 0) {
+
+        productsCart.innerHTML = "Vacio";
+        totalImport.innerHTML = "";
+    } else {
+
+        productsCart.innerHTML = "";
+        
+        for (let i = 0; i < ss.length; i++) {
+            
+            productsCart.innerHTML += "Procducto: " + ss.key(i) + " ; Cantidad: " + ss.getItem(ss.key(i)) + "<button id=" + ss.key(i) + ">X</button>" + "<br>";
+        }
+        calcTotalProducts();
+    }
+
+
+}
+
 
 
 function mostrar() {
